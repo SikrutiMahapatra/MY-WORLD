0ddb716df3b396587fcb8dc81aa1b4cca836dfaa
SikrutiMahapatra <tia10462456@gmail.com>
2025-12-09 01:32:05 +0530

    üpdated

--- project1webdev/backend/models/List.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const listSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  board: { type: mongoose.Schema.Types.ObjectId, ref: "Board", required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("List", listSchema);
